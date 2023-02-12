29adb3c3ae8a0485f1b0849fd67bd03fa6ae8b16
Gerren <voldrichv@gmail.com>
2023-02-12 10:07:33 +0100

    manifests, tools

---
+++ publish.bat
@@ -0,0 +1,8 @@
+﻿echo checklist.txt
+type "e:\Zaloha\Projects\Playnite\FilterMaster\checklist.txt"
+pause
+copy /Y "e:\Zaloha\Projects\Playnite\FilterMaster\extension.yaml" "E:\Zaloha\Projects\Playnite\FilterMaster\bin\Debug\extension.yaml"
+d:\Playnite\Toolbox.exe pack "E:\Zaloha\Projects\Playnite\FilterMaster\bin\Debug" "E:\Zaloha\Projects\Playnite"
+pause
+explorer "E:\Zaloha\Projects\Playnite"
+explorer "https://github.com/Gerren/Playnite-FilterMaster/releases/new"
---
+++ verify.bat
@@ -0,0 +1,4 @@
+﻿@echo
+d:\Playnite\Toolbox.exe verify Installer "e:\Zaloha\Projects\Playnite\FilterMaster\installer.yaml"
+d:\Playnite\Toolbox.exe verify Addon "e:\Zaloha\Projects\Playnite\FilterMaster\Gerren_MindGame.yaml"
+pause
